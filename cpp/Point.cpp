33d0e62d59e1dd126b09b50f5764b0d38fc2c6fe
Oleksandr Kunzietsov <caiman@7sky.dev>
2024-09-01 19:46:11 +0300

    struct Point

--- cpp/Point.cpp
+++ cpp/Point.cpp
@@ -4,55 +4,51 @@
 struct Point {
     int x;
     int y;
+
+    double distance(const struct Point& other) const {
+        return hypot(this->x-other.x, this->y-other.y);
+    }
+
+    bool operator==(const struct Point& other) const {
+        return this->x == other.x && this->y == other.y;
+    }
+
+    bool operator!=(const struct Point& other) const {
+        return !(*this == other);
+    }
+
+    struct Point operator+(const struct Point& other) const {
+        struct Point sum = {this->x+other.x, this->y+other.y};
+        return sum;
+    }
+
+    struct Point operator-(const struct Point& other) const {
+        struct Point diff = {this->x-other.x, this->y-other.y};
+        return diff;
+    }
 };
 
-void pointPrint(struct Point point) {
-    std::cout << "(" << point.x << ", " << point.y << ")";
-}
-
-double pointDistance(struct Point a, struct Point b) {
-    return hypot(a.x-b.x, a.y-b.y);
-}
-
-bool pointEqual(struct Point a, struct Point b) {
-    return a.x == b.x && a.y == b.y;
-}
-
-struct Point pointSum(struct Point a, struct Point b) {
-    Point sum = a;
-
-    sum.x += b.x;
-    sum.y += b.y;
-
-    return sum;
-}
-
-struct Point pointDiff(struct Point a, struct Point b) {
-    Point diff = a;
-
-    diff.x -= b.x;
-    diff.y -= b.y;
-
-    return diff;
+std::ostream& operator<<(std::ostream& out, const struct Point& point) {
+    out << "(" << point.x << ", " << point.y << ")";
+    return out;
 }
 
 int main() {
     struct Point a = {1, 10};
     struct Point b = {2, 4};
-    struct Point c = pointSum(a, b);
-    struct Point d = pointDiff(a, b);
 
-    pointPrint(a);
-    std::cout << std::endl;
-    pointPrint(b);
-    std::cout << std::endl;
-    pointPrint(c);
-    std::cout << std::endl;
-    pointPrint(d);
-    std::cout << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a.distance(b) << std::endl;
+    std::cout << a << std::endl;
 
-    std::cout << pointDistance(a, b) << std::endl;
-    std::cout << std::boolalpha << pointEqual(a, b) << std::endl;
+    std::cout << a + b << std::endl;
+    std::cout << a - b << std::endl;
+
+    if ( a == b ) {
+        std::cout << a << " is equal to " << b << std::endl;
+    } else {
+        std::cout << a << " is not equal to " << b << std::endl;
+    }
 
     return 0;
 }
